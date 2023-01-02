be85790c29227dee7e89ebac6ec294944203c263
Kyung Hyun Jo <53073097+katejo2000@users.noreply.github.com>
2023-01-02 22:34:07 +0900

    added question list and tested it in quiz screen

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_quiz/question_list.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({Key? key}) : super(key: key);
@@ -9,9 +10,12 @@ class QuizScreen extends StatelessWidget {
       child: Column(
         children: [
           const SizedBox(height: 100),
-          Text("질문 입니다"),
+          Text(questionList[0]["questionText"]),
           const SizedBox(height: 150),
-          ElevatedButton(onPressed: () {}, child: Text("대답 1")),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(questionList[0]["answers"][0]["text"]),
+          ),
           ElevatedButton(onPressed: () {}, child: Text("대답 2")),
           ElevatedButton(onPressed: () {}, child: Text("대답 3")),
           ElevatedButton(onPressed: () {}, child: Text("대답 4")),
